7efdb2b147b1c629a7d5dcd96a00dee7d8dc2574
Sameeksha purwar <69745044+sameeksha-purwar@users.noreply.github.com>
2023-02-25 19:55:26 +0530

    Add files via upload

---
+++ jquery1.js
@@ -0,0 +1,14 @@
+//Adding Style
+$("h1").css("color","red")
+$("h1").addClass("color")
+$("h1").hasClass("color")
+
+
+//Adding text
+$("h2").text("Bye")
+$("button").text("Click Me")
+$("h1").html("<em>hello</em>")
+
+//Manipulate Attributes
+$("a").attr("href","https://www.yahoo.com")
+//$("button")[2].text("dont")
