e5d62ef04a4e2d7025524c5675af0e83efafaf9e
Sameeksha purwar <69745044+sameeksha-purwar@users.noreply.github.com>
2023-02-26 08:20:10 +0530

    Add files via upload

---
+++ jquery4.js
@@ -0,0 +1,29 @@
+//Adding Animation 
+// $("button").on("click",function(){
+//     $("h1").hide()
+// })
+// $("button").on("click",function(){
+//     $("h1").show()
+// })
+
+//$("button").on("click",function(){
+//     $("h1").toggle()
+// })
+// $("button").on("click",function(){
+//     $("h1").fadeOut()
+// })
+// $("button").on("click",function(){
+//     $("h1").fadeToggle()
+// })
+// $("button").on("click",function(){
+//     $("h1").slideUp()
+// })
+// $("button").on("click",function(){
+//     $("h1").slideDown()
+// })
+// $("button").on("click",function(){
+//     $("h1").slideToggle()
+// })
+$("button").on("click",function(){
+    $("h1").animate({opacity:0.1})
+})
